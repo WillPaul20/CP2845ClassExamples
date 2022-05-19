3a4def02e3a563b3cf0aaac7669ded5bb7b8b68f
Pranjal Patra <pranjal.cna@gmail.com>
2022-05-19 13:31:46 -0230

    add Chapter 7 Code

---
+++ Chapter7/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivideClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Decimal a = Convert.ToDecimal(txtNum1.Text);
+                Decimal b = Convert.ToDecimal(txtNum2.Text);
+
+                txtDivisionOutput.Text = (a / b).ToString();                
+            }
+            catch (DivideByZeroException dbz)
+            {
+                MessageBox.Show(dbz.Message,"Zero Division Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message, fe.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace, ex.GetType().ToString(), MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+        }
+    }
+}

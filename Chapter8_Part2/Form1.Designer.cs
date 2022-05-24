e60985923ef3b086701478e352d9994c54585694
Pranjal Patra <pranjal.cna@gmail.com>
2022-05-24 10:32:50 -0230

    add chapter 8 part 2

---
+++ Chapter8_Part2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Chapter8_Part2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.btnAddStu = new System.Windows.Forms.Button();
+            this.btnDisplayEverything = new System.Windows.Forms.Button();
+            this.rtbStudents = new System.Windows.Forms.RichTextBox();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(767, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDisplay);
+            this.groupBox1.Controls.Add(this.rtbOutput);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.txtData);
+            this.groupBox1.Location = new System.Drawing.Point(7, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(327, 154);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "List<> Example";
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(8, 19);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(100, 20);
+            this.txtData.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(114, 20);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(6, 46);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(265, 102);
+            this.rtbOutput.TabIndex = 2;
+            this.rtbOutput.Text = "";
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Location = new System.Drawing.Point(196, 19);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplay.TabIndex = 3;
+            this.btnDisplay.Text = "display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(775, 425);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbStudents);
+            this.groupBox2.Controls.Add(this.btnDisplayEverything);
+            this.groupBox2.Controls.Add(this.btnAddStu);
+            this.groupBox2.Controls.Add(this.txtStuName);
+            this.groupBox2.Controls.Add(this.txtID);
+            this.groupBox2.Location = new System.Drawing.Point(340, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(412, 154);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SortedList <> Example";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(6, 18);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Location = new System.Drawing.Point(112, 18);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(100, 20);
+            this.txtStuName.TabIndex = 1;
+            // 
+            // btnAddStu
+            // 
+            this.btnAddStu.Location = new System.Drawing.Point(219, 18);
+            this.btnAddStu.Name = "btnAddStu";
+            this.btnAddStu.Size = new System.Drawing.Size(75, 23);
+            this.btnAddStu.TabIndex = 2;
+            this.btnAddStu.Text = "Add";
+            this.btnAddStu.UseVisualStyleBackColor = true;
+            this.btnAddStu.Click += new System.EventHandler(this.btnAddStu_Click);
+            // 
+            // btnDisplayEverything
+            // 
+            this.btnDisplayEverything.Location = new System.Drawing.Point(301, 19);
+            this.btnDisplayEverything.Name = "btnDisplayEverything";
+            this.btnDisplayEverything.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayEverything.TabIndex = 3;
+            this.btnDisplayEverything.Text = "Display";
+            this.btnDisplayEverything.UseVisualStyleBackColor = true;
+            this.btnDisplayEverything.Click += new System.EventHandler(this.btnDisplayEverything_Click);
+            // 
+            // rtbStudents
+            // 
+            this.rtbStudents.Location = new System.Drawing.Point(7, 46);
+            this.rtbStudents.Name = "rtbStudents";
+            this.rtbStudents.Size = new System.Drawing.Size(369, 96);
+            this.rtbStudents.TabIndex = 4;
+            this.rtbStudents.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbStudents;
+        private System.Windows.Forms.Button btnDisplayEverything;
+        private System.Windows.Forms.Button btnAddStu;
+        private System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.TabControl tabControl1;
+    }
+}
+
---
+++ Chapter8_Part2/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8_Part2
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> names = new List<string>();
+        private SortedList<int, string> students = new SortedList<int, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            names.Add(txtData.Text);
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            rtbOutput.Text = "";
+            foreach (string name in names)
+            {
+                rtbOutput.Text += name + "\n";
+            }
+        }
+
+        private void btnAddStu_Click(object sender, EventArgs e)
+        {
+            int sid = Convert.ToInt32(txtID.Text);
+            string name = txtStuName.Text;
+
+            students.Add(sid, name);
+        }
+
+        private void btnDisplayEverything_Click(object sender, EventArgs e)
+        {
+            rtbStudents.Text = "";
+            foreach(KeyValuePair<int, string> student in students)
+            {
+                rtbStudents.Text += $"ID = {student.Key} : Name = {student.Value}{Environment.NewLine}";
+            }
+        }
+    }
+}

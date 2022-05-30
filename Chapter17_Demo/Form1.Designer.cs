765b42832268add8351ddfaa19b42762ccad013e
Pranjal Patra <pranjal.cna@gmail.com>
2022-05-30 15:11:49 -0230

    add Ch 17 code

---
+++ Chapter17_Demo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Chapter17_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowCurrentDirectory = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.rtbFileContent = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnShowCurrentDirectory
+            // 
+            this.btnShowCurrentDirectory.Location = new System.Drawing.Point(13, 13);
+            this.btnShowCurrentDirectory.Name = "btnShowCurrentDirectory";
+            this.btnShowCurrentDirectory.Size = new System.Drawing.Size(141, 23);
+            this.btnShowCurrentDirectory.TabIndex = 0;
+            this.btnShowCurrentDirectory.Text = "Show Current Directory";
+            this.btnShowCurrentDirectory.UseVisualStyleBackColor = true;
+            this.btnShowCurrentDirectory.Click += new System.EventHandler(this.btnShowCurrentDirectory_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(99, 45);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(141, 20);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Filename:";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(247, 45);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(107, 23);
+            this.btnRead.TabIndex = 3;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // rtbFileContent
+            // 
+            this.rtbFileContent.Location = new System.Drawing.Point(16, 72);
+            this.rtbFileContent.Name = "rtbFileContent";
+            this.rtbFileContent.Size = new System.Drawing.Size(338, 208);
+            this.rtbFileContent.TabIndex = 4;
+            this.rtbFileContent.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(16, 287);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 327);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.rtbFileContent);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnShowCurrentDirectory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowCurrentDirectory;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.RichTextBox rtbFileContent;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Chapter17_Demo/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Chapter17_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowCurrentDirectory_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(Directory.GetCurrentDirectory(), "Current Working Dir");
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string userFileName = txtFileName.Text;
+
+            if (File.Exists(userFileName))
+            {
+                using (StreamReader reader = new StreamReader(new FileStream(userFileName, FileMode.Open)))
+                {
+                    string text = reader.ReadToEnd();
+                    rtbFileContent.Text = text;
+                }
+            }
+            else
+            {
+                MessageBox.Show($"File {userFileName} Not Found", 
+                    "File not found error", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string userFileName = txtFileName.Text;
+            using (StreamWriter writer = new StreamWriter(new FileStream(userFileName, FileMode.Create)))
+            {
+                writer.Write(rtbFileContent.Text);
+            }
+        }
+    }
+}

3e84d1670573db1365b8e03c02c6eb6509d6aaec
Pranjal Patra <pranjal.cna@gmail.com>
2022-06-08 14:04:56 -0230

    add ch20 code

--- Chapter20_EF_demo/Form1.Designer.cs
+++ Chapter20_EF_demo/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.rtbOutput2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(13, 13);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(438, 425);
+            this.rtbOutput.TabIndex = 0;
+            this.rtbOutput.Text = "";
+            // 
+            // rtbOutput2
+            // 
+            this.rtbOutput2.Location = new System.Drawing.Point(477, 13);
+            this.rtbOutput2.Name = "rtbOutput2";
+            this.rtbOutput2.Size = new System.Drawing.Size(518, 425);
+            this.rtbOutput2.TabIndex = 1;
+            this.rtbOutput2.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1061, 502);
+            this.Controls.Add(this.rtbOutput2);
+            this.Controls.Add(this.rtbOutput);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +63,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.RichTextBox rtbOutput2;
     }
 }
 
--- Chapter20_EF_demo/Form1.cs
+++ Chapter20_EF_demo/Form1.cs
@@ -22,13 +22,23 @@ namespace Chapter20_EF_demo
         private void Form1_Load(object sender, EventArgs e)
         {
             MMABooksContext context = new MMABooksContext();
-            var invoices = context.Invoices.ToList();
-            // var selectedInvoice = context.Invoices.Where(i => i.InvoiceId == 33).FirstOrDefault();
-            foreach (var invoice in invoices)
+
+            var products = context.Products.Where(p => p.UnitPrice > 57).OrderBy(p => p.UnitPrice);
+
+            foreach (var product in products)
             {
-                Debug.WriteLine(invoice);
+                rtbOutput.Text += $"{product.Description} Price = {product.UnitPrice}" + Environment.NewLine;
             }
 
+            var products2 = from product in context.Products
+                            where product.UnitPrice > 57
+                            orderby product.UnitPrice
+                            select product;
+
+            foreach (var product in products2)
+            {
+                rtbOutput2.Text += $"{product.Description} Price = {product.UnitPrice}" + Environment.NewLine;
+            }
 
         }
     }

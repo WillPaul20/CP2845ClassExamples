b29c1e4c346474f296b60300fc65e44fbc9a4f88
Pranjal Patra <pranjal.cna@gmail.com>
2022-06-13 15:07:18 -0230

    Chapter 22 Code Example

---
+++ Chapter22_DataGridView/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Chapter22_DataGridView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridProducts = new System.Windows.Forms.DataGridView();
+            this.btnLoadProducts = new System.Windows.Forms.Button();
+            this.btnProductsSortedByPrice = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPDesc = new System.Windows.Forms.TextBox();
+            this.txtPPrice = new System.Windows.Forms.TextBox();
+            this.txtPQuantity = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProducts)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridProducts
+            // 
+            this.dataGridProducts.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridProducts.Location = new System.Drawing.Point(6, 35);
+            this.dataGridProducts.Name = "dataGridProducts";
+            this.dataGridProducts.Size = new System.Drawing.Size(962, 369);
+            this.dataGridProducts.TabIndex = 0;
+            this.dataGridProducts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridProducts_CellClick);
+            // 
+            // btnLoadProducts
+            // 
+            this.btnLoadProducts.Location = new System.Drawing.Point(6, 6);
+            this.btnLoadProducts.Name = "btnLoadProducts";
+            this.btnLoadProducts.Size = new System.Drawing.Size(134, 23);
+            this.btnLoadProducts.TabIndex = 1;
+            this.btnLoadProducts.Text = "Show Products";
+            this.btnLoadProducts.UseVisualStyleBackColor = true;
+            this.btnLoadProducts.Click += new System.EventHandler(this.btnLoadProducts_Click);
+            // 
+            // btnProductsSortedByPrice
+            // 
+            this.btnProductsSortedByPrice.Location = new System.Drawing.Point(146, 6);
+            this.btnProductsSortedByPrice.Name = "btnProductsSortedByPrice";
+            this.btnProductsSortedByPrice.Size = new System.Drawing.Size(160, 23);
+            this.btnProductsSortedByPrice.TabIndex = 2;
+            this.btnProductsSortedByPrice.Text = "Show products sorted by price";
+            this.btnProductsSortedByPrice.UseVisualStyleBackColor = true;
+            this.btnProductsSortedByPrice.Click += new System.EventHandler(this.btnProductsSortedByPrice_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(982, 557);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnUpdate);
+            this.tabPage1.Controls.Add(this.txtPQuantity);
+            this.tabPage1.Controls.Add(this.txtPPrice);
+            this.tabPage1.Controls.Add(this.txtPDesc);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.dataGridProducts);
+            this.tabPage1.Controls.Add(this.btnProductsSortedByPrice);
+            this.tabPage1.Controls.Add(this.btnLoadProducts);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(974, 531);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(974, 531);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P Desc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 468);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "P Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 499);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "P Quantity";
+            // 
+            // txtPDesc
+            // 
+            this.txtPDesc.Location = new System.Drawing.Point(103, 439);
+            this.txtPDesc.Name = "txtPDesc";
+            this.txtPDesc.Size = new System.Drawing.Size(100, 20);
+            this.txtPDesc.TabIndex = 6;
+            this.txtPDesc.TextChanged += new System.EventHandler(this.txtPDesc_TextChanged);
+            // 
+            // txtPPrice
+            // 
+            this.txtPPrice.Location = new System.Drawing.Point(103, 465);
+            this.txtPPrice.Name = "txtPPrice";
+            this.txtPPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtPPrice.TabIndex = 7;
+            this.txtPPrice.TextChanged += new System.EventHandler(this.txtPPrice_TextChanged);
+            // 
+            // txtPQuantity
+            // 
+            this.txtPQuantity.Location = new System.Drawing.Point(103, 496);
+            this.txtPQuantity.Name = "txtPQuantity";
+            this.txtPQuantity.Size = new System.Drawing.Size(100, 20);
+            this.txtPQuantity.TabIndex = 8;
+            this.txtPQuantity.TextChanged += new System.EventHandler(this.txtPQuantity_TextChanged);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(231, 494);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 581);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridProducts)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridProducts;
+        private System.Windows.Forms.Button btnLoadProducts;
+        private System.Windows.Forms.Button btnProductsSortedByPrice;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtPQuantity;
+        private System.Windows.Forms.TextBox txtPPrice;
+        private System.Windows.Forms.TextBox txtPDesc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chapter22_DataGridView/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Chapter22_DataGridView.Models.DataLayer;
+
+namespace Chapter22_DataGridView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadProducts_Click(object sender, EventArgs e)
+        {
+            dataGridProducts.Columns.Clear();
+            dataGridProducts.Rows.Clear();
+            MMABooksContext context = new MMABooksContext();
+            dataGridProducts.DataSource = context.Products.ToList();
+        }
+
+        private void btnProductsSortedByPrice_Click(object sender, EventArgs e)
+        {
+            dataGridProducts.Columns.Clear();
+            dataGridProducts.Rows.Clear();
+            MMABooksContext context = new MMABooksContext();
+
+            var sortedProducts = from product in context.Products
+                                 orderby product.UnitPrice
+                                 select new
+                                 {
+                                     product.ProductCode,
+                                     product.Description,
+                                     product.UnitPrice,
+                                     product.OnHandQuantity
+                                 };
+
+            dataGridProducts.DataSource = sortedProducts.ToList();
+
+            DataGridViewButtonColumn modifyColumn = new DataGridViewButtonColumn();
+            modifyColumn.HeaderText = "";
+            modifyColumn.UseColumnTextForButtonValue = true;
+            modifyColumn.Text = "Modify";
+            dataGridProducts.Columns.Add(modifyColumn);
+
+            DataGridViewButtonColumn deleteColumn = new DataGridViewButtonColumn();
+            deleteColumn.HeaderText = "";
+            deleteColumn.UseColumnTextForButtonValue = true;
+            deleteColumn.Text = "Delete";
+            dataGridProducts.Columns.Add(deleteColumn);
+
+
+
+
+            dataGridProducts.Columns[0].HeaderText = "Product Code";
+            dataGridProducts.Columns[2].HeaderText = "Price";
+            dataGridProducts.Columns[3].HeaderText = "Quantity";
+
+            dataGridProducts.Columns[2].DefaultCellStyle.Format = "c";
+            
+            dataGridProducts.Columns[1].Width = 220;
+
+            dataGridProducts.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+            dataGridProducts.EnableHeadersVisualStyles = false;
+            dataGridProducts.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", 14, FontStyle.Bold);
+            dataGridProducts.AlternatingRowsDefaultCellStyle.BackColor = Color.PaleGoldenrod;
+            dataGridProducts.Columns[2].SortMode = DataGridViewColumnSortMode.NotSortable;
+            dataGridProducts.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleRight;
+
+
+        }
+        Products selectedProduct;
+        private void dataGridProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            // MessageBox.Show($"ROW={e.RowIndex} COL={e.ColumnIndex}","Cell Clicked");
+            MMABooksContext context = new MMABooksContext();
+            if (e.ColumnIndex == 4 || e.ColumnIndex == 5){
+                string selectedProductCode = $"{dataGridProducts.Rows[e.RowIndex].Cells[0].Value.ToString().Trim()}";
+                selectedProduct = context.Products.Find(selectedProductCode);
+            }
+            if (e.ColumnIndex == 4)
+            {
+                // Modify
+                txtPDesc.Text = selectedProduct.Description;
+                txtPPrice.Text = selectedProduct.UnitPrice.ToString();
+                txtPQuantity.Text = selectedProduct.OnHandQuantity.ToString();
+            }
+            else if(e.ColumnIndex == 5)
+            {
+                // Delete
+                context.Products.Remove(selectedProduct);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            MMABooksContext context = new MMABooksContext();
+            context.Products.Update(selectedProduct);
+            context.SaveChanges();
+        }
+
+        private void txtPDesc_TextChanged(object sender, EventArgs e)
+        {
+            selectedProduct.Description = txtPDesc.Text;
+        }
+
+        private void txtPPrice_TextChanged(object sender, EventArgs e)
+        {
+            selectedProduct.UnitPrice = Convert.ToDecimal(txtPPrice.Text);
+        }
+
+        private void txtPQuantity_TextChanged(object sender, EventArgs e)
+        {
+            selectedProduct.OnHandQuantity = Convert.ToInt32(txtPQuantity.Text);
+        }
+    }
+}
